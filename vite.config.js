e62ab2b031c845afb901c1c26ef1749173311ce6
EmaLambda <englishreformed@gmail.com>
2025-12-05 21:32:39 -0600

    Hola

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/proyecto-michis-TP3/',
   plugins: [react()],
 })
